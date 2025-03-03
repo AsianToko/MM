2b5386c3ed2f3b90a4b38b89e6ca4930dd9a7d46
MatsVDZ <matshaarlem2005@gmail.com>
2025-03-03 14:08:47 +0100

    routes aangepast

--- server.js
+++ server.js
@@ -98,9 +98,13 @@ client
     process.exit(1);
   });
 
-// Route om het inlogformulier te tonen
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "home.html"));
+// Route om homepage te tonen
+app.get("/", function (req, res) {
+  res.render("pages/home");
 });
 
 
+// about page
+app.get("/detail", function (req, res) {
+  res.render("pages/detail");
+});
