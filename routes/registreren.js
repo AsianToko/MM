ef9aa5d3a9f60d17d00ed222c479702ffc3e6dac
julius <julger04@gmail.com>
2025-04-03 13:23:10 +0200

    register route doet het eindelijk

---
+++ routes/registreren.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const xss = require("xss");
+const { MongoClient } = require("mongodb");
+
+const router = express.Router();
+const saltRounds = 10;
+
+// MongoDB connectie-instellingen
+const uri = "mongodb+srv://admin:admin@mmdb.barfq.mongodb.net/?retryWrites=true&w=majority&appName=MMdb";
+const client = new MongoClient(uri);
+
+// Registratiepagina weergeven
+router.get("/register", (req, res) => {
+  res.render("pages/register");
+});
+
+// Gebruiker registreren
+router.post("/register", async (req, res) => {
+  const username = xss(req.body.username);
+  const password = xss(req.body.password);
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const database = client.db("login");
+    const usersCollection = database.collection("login");
+
+    const existingUser = await usersCollection.findOne({ username });
+
+    if (existingUser) {
+      res.send(`<h2>Gebruikersnaam is al in gebruik</h2><a href="/register">Opnieuw proberen</a>`);
+    } else {
+      await usersCollection.insertOne({ username, password: hashedPassword });
+      res.send(`<h2>Account succesvol aangemaakt</h2><a href="/">Inloggen</a>`);
+    }
+  } catch (err) {
+    console.error("Error bij registreren:", err);
+    res.status(500).send("Interne serverfout.");
+  }
+});
+
+module.exports = router;
