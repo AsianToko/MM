2f79d025c0fff51dad62e059afbda1a5c8f4acea
Lampiaa <150828818+Lampiaa@users.noreply.github.com>
2025-04-05 15:16:41 +0200

    k

--- static/scripts/recommendation.js
+++ static/scripts/recommendation.js
@@ -1,15 +1,4 @@
 console.log("hall");
-// document.querySelector(".checkbox").forEach((el) => el.addEventListener("click", refresh));
-// const boxes = document.querySelectorAll(".checkbox");
-
-// boxes.forEach(box => {
-//   box.addEventListener("click", refresh);
-// });
-
-// function refresh() {
-//   console.log("jjkldf");
-//   document.getElementById("form").submit();
-// }
 
 document.querySelectorAll('input[name="genre[]"]').forEach(checkbox => {
   checkbox.addEventListener("change", () => {
